c04565d39aef6dd690e5691c625b91a2cd04fa07
Hakuz <Hakuz>
2021-01-10 19:34:53 +0000

    Initial commit

---
+++ src/test/java/mn/surx/UmTest.java
@@ -0,0 +1,21 @@
+package mn.surx;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class UmTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
